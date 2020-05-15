4fddf9f46365306fa62e0acd34b2adfeefc46b5e
ashwingadhave <ashwingadhave@users.noreply.github.com>
2020-05-14 20:25:49 -0400

    First Commit

---
+++ src/DoublyLinkedList.java
@@ -0,0 +1,37 @@
+public class DoublyLinkedList <T>{
+    private int size = 0;
+    private Node<T> head = null;
+    private Node<T> tail = null;
+
+    private class Node<T>{
+        T data;
+        Node <T> prev,next;
+
+        public Node(T data, Node<T> prev, Node<T> next){
+            this.data = data;
+            this.prev = prev;
+            this.next =next;
+        }
+    }
+
+    public void addNodeAtLast(T data){
+        if(head == null){
+            head = tail = new Node<T>(data,null,null);
+
+        }else if(head == tail){
+            head.next = new Node<T>(data,head,null);
+            tail = head.next;
+        }else{
+            tail.next = new Node<T>(data, tail, null);
+            tail = tail.next;
+        }
+
+    }
+
+    public void removeNode(Node<T> node){
+        while(){
+
+        }
+    }
+
+}
